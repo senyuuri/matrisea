3a89996f704864773091cf7e0c8d338c553132c2
senyuuri <senyuuri@gmail.com>
2022-02-08 04:07:29 +0000

    Improve file model form UX and extract it to an external file

--- web/src/components/NewVMForm.js
+++ web/src/components/NewVMForm.js
@@ -1,10 +1,9 @@
-import { Drawer, Form, Button, Col, Row, Input, Select, Upload, Modal, Tabs, Divider, Steps, message} from 'antd';
-import React, { useState, useCallback, useEffect, useReducer } from 'react';
-import { PlusOutlined, InboxOutlined, CheckOutlined, LoadingOutlined} from '@ant-design/icons';
+import { Drawer, Form, Button, Col, Row, Input, Select, Divider, Steps, message} from 'antd';
+import React, { useState, useCallback, useEffect, useReducer, useRef} from 'react';
+import { PlusOutlined, CheckOutlined, LoadingOutlined} from '@ant-design/icons';
 import { WsContext } from '../Context';
+import FileModalForm from './FileModalForm';
 
-const { TabPane } = Tabs;
-const { Dragger } = Upload;
 const { Option } = Select;
 const { Step } = Steps;
 
@@ -204,11 +203,14 @@ function NewVMForm(props) {
           // will be synchronised to the main form
           onFormFinish={(name, { values, forms }) => {
             if (name === 'fileForm') {
-              const { mainForm } = forms;
+              const { mainForm, fileForm } = forms;
               if(filePickerType === "System") {
                 mainForm.setFieldsValue({
                   system_image: values.filename
                 });
+                if (values.filename.length > 35) {
+                  values.filename = values.filename.slice(0,15) + "..." + values.filename.slice(-12)
+                } 
                 setSystemImageButtonText(values.filename);
                 setSystemImageIcon('CheckOutlined');
               }
@@ -216,9 +218,13 @@ function NewVMForm(props) {
                 mainForm.setFieldsValue({
                   cvd_image: values.filename
                 })
+                if (values.filename.length > 35) {
+                  values.filename = values.filename.slice(0,15) + "..." + values.filename.slice(-12)
+                } 
                 setCvdImageButtonText(values.filename);
                 setCvdImageIcon('CheckOutlined');
               }
+              fileForm.resetFields();
               setFileModalVisible(false);
             }
           }}
@@ -231,6 +237,7 @@ function NewVMForm(props) {
             hideRequiredMark
             onFinish={submitForm}
             initialValues={{
+              name: 'cvd-'+ Math.random().toString(36).substring(2, 8),
               type: "cuttlefish-kvm",
               cpu: 2,
               ram: 4,
@@ -341,7 +348,7 @@ function NewVMForm(props) {
           </Form>
           <FileModalForm 
             visible={fileModalVisible} 
-            onCancel={hideFileModal} 
+            onCancelCallback={hideFileModal} 
             target={filePickerType}
             fileList={fileList}
           />
@@ -365,68 +372,5 @@ function NewVMForm(props) {
 		</Drawer>
 	)
 }
-
-const FileModalForm = ({ visible, onCancel, target, fileList }) => {
-  const API_ENDPOINT = window.location.protocol+ "//"+  window.location.hostname + ":" + process.env.REACT_APP_API_PORT + "/api/v1"
-  const [form] = Form.useForm();
-  
-  const onOk = () => {
-    form.submit();
-    onCancel();
-  };
-
-  const draggerProps = {
-    name: 'file',
-    multiple: false,
-    accept: ".zip,.tar",
-    action: API_ENDPOINT + "/files/upload",
-    onChange(info) {
-      const { status } = info.file;
-      if (status !== 'uploading') {
-        //console.log(info.file, info.fileList);
-      }
-      if (status === 'done') {
-        message.success(`${info.file.name} file uploaded successfully.`);
-        form.setFieldsValue({filename: info.file.name});
-      } else if (status === 'error') {
-        message.error(`${info.file.name} file upload failed.`);
-      }
-    },
-    onDrop(e) {
-      console.log('Dropped files', e.dataTransfer.files);
-    }
-  }
-
-  return (
-    <Modal title={"Select " +target +" File"} visible={visible} onOk={onOk} onCancel={onCancel}>
-
-      <Tabs defaultActiveKey="1">
-          <TabPane tab="Choose Image" key="1">
-          <Form form={form} layout="vertical" name="fileForm">
-            <Form.Item name="filename">
-              <Select
-                showSearch
-                style={{ width: '100%' }}
-                options={fileList}
-              />
-            </Form.Item>
-          </Form>
-          </TabPane>
-          <TabPane tab="Upload New Image" key="2">
-          <Dragger {...draggerProps}>
-            <p className="ant-upload-drag-icon">
-              <InboxOutlined />
-            </p>
-            <p className="ant-upload-text">Click or drag .zip/.tar to this area to upload</p>
-          </Dragger>
-          </TabPane>
-          <TabPane tab="Pick from Android CI" key="3" disabled={true}>
-            Content of Tab Pane 3
-          </TabPane>
-      </Tabs>
-      
-    </Modal>
-  );
-};
 
 export default NewVMForm;
